59c6068338a402fff954c06f6331e8627558aa2a
samuelprasad-yubi <samuel.prasad@spocto.com>
2023-07-30 02:16:33 +0530

    remote url

--- helpers.js
+++ helpers.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const baseUrlRemote = "https://dark-red-catfish-tux.cyclic.app";
 export const baseUrlLocal = "http://localhost:3000";
 
-export const getBaseUrl = () => baseUrlLocal;
+export const getBaseUrl = () => baseUrlRemote;
 export const url = `${getBaseUrl()}/nearby`;
 export const fetchData = async (latitude, longitude) => {
   console.log(">>>>> fetchData", { latitude, longitude });
