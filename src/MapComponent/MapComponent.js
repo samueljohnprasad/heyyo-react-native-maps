58a2534d248c3e00f99ca7b2c06aafdc4d5d865f
samuelprasad-yubi <samuel.prasad@spocto.com>
2023-07-30 16:23:38 +0530

    clusters multiple post at same locations

--- src/MapComponent/MapComponent.js
+++ src/MapComponent/MapComponent.js
@@ -1,11 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState, useRef } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  Dimensions,
+  Pressable,
+  TouchableHighlight,
+} from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { useQuery } from "react-query";
 import { fetchData } from "../../helpers";
 import axios from "axios";
 import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function MapComponent({ latitude, longitude }) {
   // const {
@@ -16,9 +25,13 @@ export default function MapComponent({ latitude, longitude }) {
   //   fetchData(latitude, longitude)
   // );
   const mapRef = useRef(null);
+  const { width, height } = Dimensions.get("window");
+  const CARD_HEIGHT = height / 4;
+  const CARD_WIDTH = CARD_HEIGHT - 50;
 
   const [userCurrentLocation, setUserCurrentLocation] = useState([]);
   const { navigate } = useNavigation();
+  const [sliderPosts, setSliderPosts] = useState([]);
   useEffect(() => {
     const getDate = async () => {
       console.log({ latitude, longitude });
@@ -36,6 +49,10 @@ export default function MapComponent({ latitude, longitude }) {
     mapRef.current.animateToRegion(region, 1 * 1000);
   }, [latitude, longitude]);
 
+  const onPressSliderPostsBack = () => {
+    setSliderPosts([]);
+  };
+
   // if (isLoading) {
   //   return (
   //     <View style={styles.container}>
@@ -48,6 +65,14 @@ export default function MapComponent({ latitude, longitude }) {
   //console.log(">>>>>>> data", { data: data?.data });
   // console.log(">>>>>>> location", { data: data[0].location.coordinates });
   console.log(">>>>>>> data", { data: userCurrentLocation });
+  const onMapPostClick = (cluster) => {
+    if (cluster.length == 1) return navigate("PostOverViewModal");
+    setSliderPosts(cluster);
+  };
+
+  const onPressSliderCard = () => {
+    navigate("PostOverViewModal");
+  };
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -68,25 +93,82 @@ export default function MapComponent({ latitude, longitude }) {
         userLocationAnnotationTitle="this is my location"
         style={styles.map}
       >
-        {userCurrentLocation.map((map, index) => {
+        {userCurrentLocation.map((cluster, index) => {
+          console.log("cluster XXXXXX", { cluster });
+          console.log("zzzzzzzzzzzzzzzz", cluster?.length);
           return (
             <Marker
               key={index}
               tracksViewChanges
               coordinate={{
-                latitude: map.location.coordinates[1],
-                longitude: map.location.coordinates[0],
+                // latitude: map.location.coordinates[1],
+                latitude:
+                  cluster.reduce(
+                    (acc, image) => acc + image.location.coordinates[1],
+                    0
+                  ) / cluster.length,
+
+                longitude:
+                  cluster.reduce(
+                    (acc, image) => acc + image.location.coordinates[0],
+                    0
+                  ) / cluster.length,
               }}
-              onSelect={() => navigate("PostOverViewModal")}
-              onPress={() => console.log("on onPress >>>>>")}
-              title={map.userName}
-              description={map.message}
+              //onSelect={() => onMapPostClick(cluster)}
+              onPress={() => onMapPostClick(cluster)}
+              title={cluster.length.toString()}
+              // description={map.message}
             >
               <View style={styles.roundView}></View>
             </Marker>
           );
         })}
       </MapView>
+      {!!sliderPosts.length && (
+        <Animated.ScrollView
+          horizontal
+          pagingEnabled
+          scrollEventThrottle={1}
+          showsHorizontalScrollIndicator={false}
+          snapToInterval={CARD_WIDTH}
+          style={styles.scrollView}
+        >
+          <View
+            style={{
+              flexDirection: "column",
+              gap: 10,
+              marginHorizontal: 10,
+            }}
+          >
+            <View
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: "50%",
+                backgroundColor: "white",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Pressable onPress={onPressSliderPostsBack}>
+                <Ionicons name="ios-chevron-back" size={24} color="black" />
+              </Pressable>
+            </View>
+            <View style={{ flexDirection: "row" }}>
+              {sliderPosts.map((post, index) => {
+                return (
+                  <Pressable key={index} onPress={onPressSliderCard}>
+                    <View style={styles.card}>
+                      <Text>{post.userName}</Text>
+                      <Text>{post.message}</Text>
+                    </View>
+                  </Pressable>
+                );
+              })}
+            </View>
+          </View>
+        </Animated.ScrollView>
+      )}
     </View>
   );
 }
@@ -121,5 +203,26 @@ const styles = StyleSheet.create({
     overflow: "visible",
     borderColor: "black",
     borderWidth: 2,
+  },
+  card: {
+    padding: 10,
+    elevation: 2,
+    backgroundColor: "#FFF",
+    marginHorizontal: 10,
+    shadowColor: "#000",
+    shadowRadius: 5,
+    shadowOpacity: 0.3,
+    shadowOffset: { x: 2, y: -2 },
+    height: 200,
+    width: 200,
+    overflow: "hidden",
+    borderRadius: 10,
+  },
+  scrollView: {
+    position: "absolute",
+    bottom: 100,
+    left: 0,
+    right: 0,
+    paddingVertical: 10,
   },
 });
