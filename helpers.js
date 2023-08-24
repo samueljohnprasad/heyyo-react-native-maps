c6ac995c0282dd76148893f3247ce989add02187
samuelprasad-yubi <samuel.prasad@spocto.com>
2023-08-24 22:26:22 +0530

    url

--- helpers.js
+++ helpers.js
@@ -4,22 +4,23 @@ import Toast from "react-native-toast-message";
 export const baseUrlRemote = "https://dark-red-catfish-tux.cyclic.app";
 export const baseUrlLocal = "http://localhost:3000";
 
-export const getBaseUrl = () => baseUrlRemote;
-export const url = `${getBaseUrl()}/nearby`;
-export const fetchData = async (latitude, longitude) => {
-  try {
-    const response = await axios.get(url, {
-      params: {
-        latitude,
-        longitude,
-      },
-    });
+export const getBaseUrl = () => baseUrlLocal;
+//export const url = `${getBaseUrl()}/nearby`;
+// export const fetchData = async (latitude, longitude) => {
+//     try {
+//         const response = await axios.get(url, {
+//             params: {
+//                 latitude,
+//                 longitude,
+//             },
+//         });
 
-    return response.data;
-  } catch (e) {
-    Toast.show({
-      type: "error",
-      text1: "error in fetching nearby posts",
-    });
-  }
-};
+//         return response.data;
+//     } catch (e) {
+//         console.log(e);
+//         Toast.show({
+//             type: "error",
+//             text1: "error in fetching nearby posts",
+//         });
+//     }
+// };
--- src/network/socket.js
+++ src/network/socket.js
@@ -1,2 +1,3 @@
 import io from "socket.io-client";
-export const socket = io("http://localhost:3000");
+import { getBaseUrl } from "../../helpers";
+export const socket = io(getBaseUrl());
