4ed89cb8c6a9a0fe67456e9be65af67e3d4eda6c
samuelprasad-yubi <samuel.prasad@spocto.com>
2023-08-25 11:11:41 +0530

    socket reconnection

--- src/network/socket.js
+++ src/network/socket.js
@@ -1,3 +1,15 @@
 import io from "socket.io-client";
 import { getBaseUrl } from "../../helpers";
-export const socket = io(getBaseUrl());
+import Toast from "react-native-toast-message";
+
+export const socket = io(getBaseUrl(), {
+    reconnectionAttempts: 3,
+    reconnectionDelay: 1000,
+    reconnectionDelayMax: 5000,
+});
+socket.on("connect_error", (error) => {
+    Toast.show({
+        type: "error",
+        text1: "Socket connection error",
+    });
+});
--- src/store/sockets.js
+++ src/store/sockets.js
@@ -1,5 +1,6 @@
 import { socket } from "../network/socket";
 import { updateNewPosts } from "./post.reducer";
+import Toast from "react-native-toast-message";
 
 export const realTimeNewPostUpdates = () => (dispatch) => {
     socket.on("realTimeNewPostUpdates", (data) => {
@@ -14,7 +15,13 @@ export const postNewPost =
             if (data.status == 200) {
                 callBackFunction();
                 dispatch(updateNewPosts({ post: data.newPost }));
+            } else {
+                Toast.show({
+                    type: "error",
+                    text1: "error in fetching new post",
+                });
             }
         };
+
         socket.emit("postNewPost", newPost, callbackFun);
     };
